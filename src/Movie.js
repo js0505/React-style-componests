97bae72388b06dde4e7bb9379ba1b078855decf3
jisu <shinjs0505@naver.com>
2021-07-01 13:54:51 +0900

    Comment, Ready to review

--- src/Movie.js
+++ src/Movie.js
@@ -3,32 +3,35 @@ import "./Movie.css";
 import Poster from './Poster';
 import LinesEllipsis from 'react-lines-ellipsis';
 
+// App.js에서 movie state의 props로 데이터를 받아서 
+// 포스터, 제목, 장르 등의 필요한 데이터를 가공하는 컴포넌트
 
 const Movie = ({item}) => {
     return (
         <div className={"Movie"}>
-         <div className={"Movie_Column"}>
-          <Poster src={item.poster_path} alt={item.title} />
-         </div>
-         <div className={"Movie_Column"}>
-           <h1>{item.title}</h1>
-           <div className={"Movie_Genres"}>
-             {item.genre_ids.map(genre => (
-               <span className={"Movie_Genre"}>{genre}</span>
-              ))}
-           </div>
-           <div className={"Movie_Overview"}>
-             {/* <span>{item.overview.slice(0, 200)} ...</span> */}
-             <LinesEllipsis
-                text={item.overview}
-                maxLine='3'
-                ellipsis='...'
-                trimRight
-                basedOn='letters'
-             />
-           </div>
-         </div>
-       </div> 
+          <div className={"Movie_Column"}>
+            <Poster src={item.poster_path} alt={item.title} />
+          </div>
+
+          <div className={"Movie_Column"}>
+            <h1>{item.title}</h1>
+            <div className={"Movie_Genres"}>
+              {item.genre_ids.map(genre => (
+              <span className={"Movie_Genre"}>{genre}</span>
+                ))}
+            </div>
+              <div className={"Movie_Overview"}>
+                {/* <span>{item.overview.slice(0, 200)} ...</span> */}
+                  <LinesEllipsis
+                    text={item.overview}
+                    maxLine='3'
+                    ellipsis='...'
+                    trimRight
+                    basedOn='letters'
+                  />
+              </div>
+          </div>
+        </div> 
     );
 };
 
--- src/Poster.js
+++ src/Poster.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Movie.css'
 
 
+// 포스터 데이터가 파일명만 존재해서 다른 url에 입력해서 받아와야 하므로 
+// 컴포넌트로 따로 분리.
 const Poster = ({src, alt}) => {
     return (
         <img src={`https://image.tmdb.org/t/p/w500${src}`} alt={alt} className={"Movie_Poster"} />
